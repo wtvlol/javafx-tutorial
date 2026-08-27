aea7c8aba05b3c69951ad65ea430f8fec24c466e
wtvlol <keithchiawk@gmail.com>
2026-08-27 23:02:03 +0800

    Tutorial 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,4 +2,10 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
 }
